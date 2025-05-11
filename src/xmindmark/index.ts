97efbfc987d9900cc08b57193d9e45aa29bf0cb3
jinzcdev <jinzcdev@gmail.com>
2025-05-11 15:27:01 +0800

    feat: add exports for `xmindmark` module

---
+++ src/xmindmark/index.ts
@@ -0,0 +1,4 @@
+export { createMapByXMindMark } from './parser/mindmark'
+export { parseXMindMarkToXMindFile } from './lib/xmindmark-to-xmind'
+export { parseXMindToXMindMarkFile } from './lib/xmind-to-xmindmark'
+export * from './types'
